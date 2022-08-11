e35884b49cc5bc6610ea4b769b820f203d491723
TakayukiTooyama <t.tooyama107@gmail.com>
2022-08-11 14:18:42 +0900

    Three.jsの共通処理の追記

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 import "./style.css";
+import * as THREE from "three";
+
+const canvas = document.querySelector("#webgl");
+
+const scene = new THREE.Scene();
+
+const sizes = {
+  width: innerWidth,
+  height: innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 0.1, 500);
+const directionalLight = new THREE.DirectionalLight();
+scene.add(directionalLight);
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.getPixelRatio(window.devicePixelRatio);
+
+const tick = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
+
+window.addEventListener("resize", () => {
+  sizes.width = innerWidth;
+  sizes.height = innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.getPixelRatio(window.devicePixelRatio);
+});
